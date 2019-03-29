7bdd1010fd9f1817da4f6104adf0539b48c6d382
tulasi <tulasipinjala.com>
2019-03-29 12:54:36 -0400

    Read me

---
+++ kindergarden-Api/src/main/java/org/wecancodeit/kindergarden/KindergardenApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.kindergarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindergardenApplication.class, args);
+	}
+
+}
